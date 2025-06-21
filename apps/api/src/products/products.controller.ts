d1e770beadc2e735e01cd20d3a068d0355d5e0cd
smoxx <erik.lenhardt@protonmail.com>
2025-06-21 21:22:15 +0200

    refactor(api): integrate @repo/types for product creation and update types package structure

--- apps/api/src/products/products.controller.ts
+++ apps/api/src/products/products.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { CreateProductRequest } from '@repo/types';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  async createProduct(@Body() createProductRequest) {}
+  async createProduct(@Body() createProductRequest: CreateProductRequest) {}
 }
