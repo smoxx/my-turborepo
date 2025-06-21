10ccb17d20d4a5514e0210305181df9f4c0a43ee
smoxx <erik.lenhardt@protonmail.com>
2025-06-21 22:32:27 +0200

    github workflow and testing changes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,8 @@
 import CreateProduct from "./create-product/create-product";
 import Products from "./products/products";
 
+export const dynamic = "force-dynamic"
+
 export default function Page() {
   return (
     <div className="flex flex-col gap-10 max-w-md mx-auto py-10">
