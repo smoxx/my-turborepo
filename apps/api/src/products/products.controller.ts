383e0ed32150c107242d841c8ea825bd4b486b6a
smoxx <erik.lenhardt@protonmail.com>
2025-06-21 21:08:14 +0200

    added a products module controller and service for nest and a types package

---
+++ apps/api/src/products/products.controller.ts
@@ -0,0 +1,10 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  async createProduct(@Body() createProductRequest) {}
+}
